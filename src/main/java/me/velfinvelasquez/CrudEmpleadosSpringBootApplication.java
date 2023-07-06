ef2e23be564e2952f80cdace76405f9233deebb5
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-07-05 19:53:58 -0500

    init

---
+++ src/main/java/me/velfinvelasquez/CrudEmpleadosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package me.velfinvelasquez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmpleadosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmpleadosSpringBootApplication.class, args);
+	}
+
+}
