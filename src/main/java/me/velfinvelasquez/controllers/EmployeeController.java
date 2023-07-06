d2026cdda1db74a886d6c7527fd2ed2a8d5ed384
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-07-05 23:03:06 -0500

    endpoint nuevo empleado

--- src/main/java/me/velfinvelasquez/controllers/EmployeeController.java
+++ src/main/java/me/velfinvelasquez/controllers/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,19 @@ import me.velfinvelasquez.models.Employee;
 import me.velfinvelasquez.repository.EmployeeRepository;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/employees")
 @CrossOrigin(origins = {"http://localhost:4200/"})
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	@GetMapping("/employees")
+	@GetMapping()
 	public List<Employee> listAllEmployees(){
 		return employeeRepository.findAll();
+	}
+	
+	@PostMapping()
+	public Employee create(@RequestBody Employee employee){
+		return employeeRepository.save(employee);
 	}
 }
